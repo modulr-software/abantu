357abb70a2866813d59bb662da5ac66aec96fbd0
merveillevaneck <merveillevaneck@gmail.com>
2026-08-18 21:11:56 +0200

    added example of exercises api using malt

---
+++ src/abantu/services/exercises/interface.clj
@@ -0,0 +1,118 @@
+(ns abantu.services.exercises.interface
+  (:require [io.julienvincent.malt :as malt]
+            [malli.util :as mu]
+            [abantu.db.interface :as db]))
+
+(def ?Option :string)
+(def ?Answer :string)
+
+(def ?Exercise
+  [:map
+   [:id :int]
+   [:unit-id :int]
+   [:course-id :int]
+   [:level :int]
+   [:order :int]
+   [:options [:vector ?Option]]
+   [:answer-type :string]
+   [:instruction :string]
+   [:question-content :string]
+   [:correct-message :string]
+   [:answers [:vector ?Answer]]])
+
+(def ?Lookup
+  (mu/select-keys ?Exercise [:id :unit-id :course-id]))
+
+(def ?Find
+  ?Lookup)
+
+(def ?SetUnit
+  (mu/select-keys ?Exercise [:id :unit-id]))
+
+(def ?SetInstruction
+  (mu/select-keys ?Exercise [:id :instruction]))
+
+(def ?SetQuestionContent
+  (mu/select-keys ?Exercise [:id :qestion-content]))
+
+(def ?SetAnswerType
+  (mu/select-keys ?Exercise [:id :answer-type]))
+
+(def ?SetLevel
+  (mu/select-keys ?Exercise [:id :level]))
+
+(def ?SetCorrectMessage
+  (mu/select-keys ?Exercise [:id :correct-message]))
+
+(def ?SetIncorrectMessage
+  (mu/select-keys ?Exercise [:id :incorrect-message]))
+
+(def ?SetOrder
+  (mu/select-keys ?Exercise [:id :order]))
+
+(def ?SetOptions
+  (mu/select-keys ?Exercise [:id :options]))
+
+(def ?RemoveOption
+  (-> (mu/select-keys ?Exercise [:id])
+      (mu/assoc :option ?Option)))
+
+(def ?AddOption
+  (-> (mu/select-keys ?Exercise [:id])
+      (mu/assoc :option ?Option)))
+
+(def ?AddAnswer
+  (-> (mu/select-keys ?Exercise [:id])
+      (mu/assoc :answer ?Answer)))
+
+(def ?RemoveAnswer
+  (-> (mu/select-keys ?Exercise [:id])
+      (mu/assoc :answer ?Answer)))
+
+(def ?SetAnswers
+  (mu/select-keys ?Exercise [:id :answers]))
+
+(malt/defprotocol Exercises
+  (lookup [input ?Lookup]
+    ?Exercise)
+  (find [input ?Find]
+    [:vector ?Exercise])
+  (set-unit [input ?SetUnit]
+    :boolean)
+  (set-instruction [input ?SetInstruction]
+    :boolean)
+  (set-question-content [input ?SetQuestionContent]
+    :boolean)
+  (set-answer-type [input ?SetAnswerType]
+    :boolean)
+  (set-level [input ?SetLevel]
+    :boolean)
+  (set-correct-message [input ?SetCorrectMessage]
+    :boolean)
+  (set-incorrect-message [input ?SetCorrectMessage]
+    :boolean)
+  (set-order [input ?SetOrder]
+    :boolean)
+  (set-options [input ?SetOptions]
+    :boolean)
+  (add-option [input ?AddOption]
+    :boolean)
+  (remove-option [input ?RemoveOption]
+    :boolean)
+  (set-answers [input ?SetAnswers]
+    :boolean)
+  (add-answer [input ?AddAnswer]
+    :boolean)
+  (remove-answer [input ?RemoveAnswer]
+    :boolean))
+
+(defn- -lookup [{:keys [id unit-id]}]
+  (let [ds (db/ds :master)]
+    (db/find-one ds {:tname :exercises
+                     :where [:= :id id]})))
+
+(defn create-service []
+  (let [ds (db/ds :master)]
+    (malt/reify Exercises
+      (lookup [_ input]
+        (-lookup input)))))
