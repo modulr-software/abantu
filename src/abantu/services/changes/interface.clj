77fdf1aaa0de2d29f9db43c76a28916feb15c63f
Keagan <keaganncollins@gmail.com>
2026-08-21 16:21:00 +0200

    added malli schemas

--- src/abantu/services/changes/interface.clj
+++ src/abantu/services/changes/interface.clj
@@ -2,6 +2,30 @@
   (:require [io.julienvincent.malt :as malt]
             [malli.util :as mu]))
 
+(def ?CourseChanges
+  [:map
+   [:course-id :int]
+   [:change-type :string]
+   [:change-data :string]
+   [:timestamp :string]])
+
+(def ?UnitChanges
+  [:map
+   [:course-id :int]
+   [:unit-id :int]
+   [:change-type :string]
+   [:change-data :string]
+   [:timestamp :string]])
+
+(def ?ExerciseChanges
+  [:map
+   [:exercise-id :int]
+   [:course-id :int]
+   [:unit-id :int]
+   [:change-type :string]
+   [:change-data :string]
+   [:timestamp :string]])
+
 (malt/defprotocol VersionControlQuery
   (lookup [input])
   (find [input])
