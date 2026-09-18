622bda4627efa9a39db8256927e7e0d3e3f687ce
Keagan <keaganncollins@gmail.com>
2026-09-18 11:59:45 +0200

    added test data in json

--- test/abantu/exercises_interface_test.clj
+++ test/abantu/exercises_interface_test.clj
@@ -1,8 +1,16 @@
 (ns abantu.exercises-interface-test
   (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [clojure.data.json :as json]
+            [clojure.string :as str]
             [abantu.db-test-utils :as db-test-utils]
             [abantu.db.honey :as hon]
             [abantu.services.exercises.interface :as exercises-intf-sut]))
+
+(def exercises-data
+  (-> (io/resource "abantu/exercises.json")
+      slurp
+      (json/read-str {:key-fn keyword})))
 
 (def control-ds (atom nil))
 (def sut-ds (atom nil))
@@ -20,50 +28,51 @@
            (db-test-utils/cleanup-test-db! 1)
            (db-test-utils/cleanup-test-db! 2)))))
 
+(defn mirror-create!
+  "Insert into control-ds exactly the rows -create writes for `input`."
+  [input created]
+  (hon/insert! @control-ds
+               {:tname :exercises
+                :values {:uuid (:uuid created)
+                         :unit-id (:unit-id input)
+                         :course-id (:course-id input)
+                         :instruction (:instruction input)
+                         :question-content (:question-content input)
+                         :answer-type (:answer-type input)
+                         :options (str/join ";;" (:options input))
+                         :correct-message (:correct-message input)
+                         :incorrect-message (:incorrect-message input)}})
+  (when (seq (:answers input))
+    (hon/insert! @control-ds
+                 {:tname :answers
+                  :values (mapv #(assoc {:text (str/join ";;" %)}
+                                        :exercise-id (:id created))
+                                (:answers input))})))
+
+(defn seed-exercise!
+  "Create `input` via the interface in sut-ds and mirror its rows into control-ds."
+  [input]
+  (let [created (exercises-intf-sut/create @em input)]
+    (mirror-create! input created)
+    created))
+
+(defn assert-control-matches!
+  "Assert the exercises and answers tables are identical between the two dbs."
+  []
+  (is (= (hon/find @control-ds {:tname :exercises :ret :*})
+         (hon/find @sut-ds {:tname :exercises :ret :*})))
+  (is (= (hon/find @control-ds {:tname :answers :ret :*})
+         (hon/find @sut-ds {:tname :answers :ret :*}))))
+
 (deftest create-test
   (testing "manual control insert and interface create leave identical db contents"
-    (let [input {:unit-id 1
-                 :course-id 1
-                 :instruction "Translate the following"
-                 :question-content "Who are you?"
-                 :answer-type "bubbles"
-                 :options ["wat" "wie" "hoe"]
-                 :correct-message "correct!"
-                 :incorrect-message "o nei"
-                 :answers [["wie" "is"]]}
-          created (exercises-intf-sut/create @em input)
-          ;; control insert mirrors exactly what -create writes to the db
-          _inserted-exercise (hon/insert! @control-ds
-                                          {:tname :exercises
-                                           :values {:uuid (:uuid created)
-                                                    :unit-id 1
-                                                    :course-id 1
-                                                    :instruction "Translate the following"
-                                                    :question-content "Who are you?"
-                                                    :answer-type "bubbles"
-                                                    :options "wat;;wie;;hoe"
-                                                    :correct-message "correct!"
-                                                    :incorrect-message "o nei"}})
-          _inserted-answer (hon/insert! @control-ds
-                                        {:tname :answers
-                                         :values {:exercise-id (:id created)
-                                                  :text "wie;;is"}})]
-      (is (= (hon/find @control-ds {:tname :exercises :ret :*})
-             (hon/find @sut-ds {:tname :exercises :ret :*})))
-      (is (= (hon/find @control-ds {:tname :answers :ret :*})
-             (hon/find @sut-ds {:tname :answers :ret :*}))))))
+    (let [input (exercises-data :bubbles-basic)
+          _created (seed-exercise! input)]
+      (assert-control-matches!))))
 
 (deftest delete-test
   (testing "delete removes the exercise and all its dependent rows"
-    (let [input {:unit-id 1
-                 :course-id 1
-                 :instruction "Translate the following"
-                 :question-content "Who are you?"
-                 :answer-type "bubbles"
-                 :options ["wat" "wie" "hoe"]
-                 :correct-message "correct!"
-                 :incorrect-message "o nei"
-                 :answers [["wie"] ["hoe"]]}
+    (let [input (exercises-data :bubbles-multi)
           created (exercises-intf-sut/create @em input)
           id (:id created)
           _comment (hon/insert! @sut-ds {:tname :comments
@@ -91,3 +100,34 @@
       (is (empty? (hon/find @sut-ds {:tname :exercises-completed
                                      :where [:= :exercise-id id]
                                      :ret :*}))))))
+
+(deftest lookup-test
+  (testing "lookup returns the seeded exercise by id and by uuid"
+    (let [input (exercises-data :bubbles-basic)
+          created (seed-exercise! input)
+          by-id (exercises-intf-sut/lookup @eq {:id (:id created)})
+          by-uuid (exercises-intf-sut/lookup @eq {:uuid (:uuid created)})]
+      (assert-control-matches!)
+      (is (= (:id created) (:id by-id)))
+      (is (= (:uuid created) (:uuid by-id)))
+      (is (= (:id created) (:id by-uuid)))
+      (is (= (:uuid created) (:uuid by-uuid))))))
+
+(deftest find-test
+  (testing "find returns exercises by course and by unit"
+    (let [basic (seed-exercise! (exercises-data :bubbles-basic))
+          unit-two (seed-exercise! (exercises-data :bubbles-unit-two))]
+      (assert-control-matches!)
+      (is (= 2 (count (exercises-intf-sut/find @eq {:course-id 1}))))
+      (is (= #{(:id basic)}
+             (set (map :id (exercises-intf-sut/find @eq {:unit-id 1})))))
+      (is (= #{(:id unit-two)}
+             (set (map :id (exercises-intf-sut/find @eq {:unit-id 2}))))))))
+
+(deftest all-test
+  (testing "all returns every seeded exercise"
+    (let [basic (seed-exercise! (exercises-data :bubbles-basic))
+          unit-two (seed-exercise! (exercises-data :bubbles-unit-two))]
+      (assert-control-matches!)
+      (is (= #{(:id basic) (:id unit-two)}
+             (set (map :id (exercises-intf-sut/all @eq))))))))
